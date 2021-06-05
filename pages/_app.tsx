14f96feb4b4ca865d32e828af645dee8594ca573
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-06-05 19:23:19 +0700

    Setup tailwind for styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
 
+import "tailwindcss/tailwind.css";
+
 function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import { FunctionComponent } from "react";
-
 import Head from "next/head";
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const Page: FunctionComponent = () => {
     return (
-        <>
-            <Head>
-                <title>Milky Safe Assets</title>
-            </Head>
-
-            <p>Hello</p>
-        </>
+        <div className="bg-black flex h-screen">
+            <Canvas className="canvas">
+                <pointLight
+                    position={[-10, 0, -20]}
+                    color="red"
+                    intensity={2.5}
+                />
+            </Canvas>
+        </div>
     );
 };
 
