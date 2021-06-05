2ccbea2d82c5393444e69d9f0f49175ceea75c90
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-06-05 17:38:42 +0700

    Rename .js to .tsx

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+
+import Head from "next/head";
+
+const Page: FunctionComponent = () => {
+    return (
+        <>
+            <Head>
+                <title>Milky Safe Assets</title>
+            </Head>
+
+            <p>Hello</p>
+        </>
+    );
+};
+
+export default Page;
